cd52a9cc6294324c179dc91d560514b9178f3634
sorai-loop <adventurer.skydreamer@gmail.com>
2026-03-01 06:09:04 +0900

    カウンター作成

--- day1/my-app/app/page.tsx
+++ day1/my-app/app/page.tsx
@@ -1,7 +1,20 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen items-center justify-center">
-      <h1 className="text-4xl font-bold">Hellow React!?!?!?</h1>
+"use client";
+
+import { useState} from "react";
+
+export default function Home(){
+  const [count ,setCount] = useState(0);
+
+  return(
+    <main className="flex min-h-screen flex-col items-center justify-center gap-6">
+      <h1 className="text-4xl font-cold">カウンター</h1>
+      <p className="text2xl">{count}</p>
+      <button
+        className="px-4 py-2 rounded-xl bg-black text-white"
+        onClick={() => setCount(count + 1)}
+        >
+        +1
+        </button>
     </main>
   );
 }
